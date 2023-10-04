67c7078a68fba10fbadc4c2773f9900ee048bbc8
Kirill <workiteen@gmail.com>
2023-10-04 05:33:19 +0500

    add_sequelize

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,77 @@
 /* eslint-disable */
-import express, {request} from 'express';
+import express from 'express';
 import * as process from 'process'
 import dotenv from 'dotenv'
 import log4js from 'log4js'
-import mysql2 from 'mysql2';
 
-//connect DB MySQL
-const mysql = require("mysql2");
+//SEQUELIZE
+////////////////////////////////////////
+const {
+    Sequelize,
+    DataTypes
+} = require('sequelize');
+export const sequelize = new Sequelize({
+    database: 'my_db',
+    username: 'root',
+    password: 'admin',
+    host: 'localhost',
+    dialect: 'mysql',
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "my_db",
-    password: "admin"
 });
 
-connection.connect(function(err){
-    if (err) {
-        return console.error("Ошибка: " + err.message);
+async function conSequelize() {
+    try {
+        await sequelize.authenticate();
+        console.log('Соединение прошло успешно.');
+    } catch (error) {
+        console.error('Невозможно подключиться к базе данных:', error);
     }
-    else{
-        console.log("Подключение к серверу MySQL успешно установлено");
-    }
-});
+}
 
+conSequelize();
+////////////////////////////////
+///////////////////////////////////////////
+const User = sequelize.define('User', {
+    // Описание модели пользователей
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    phone: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: true
+    }
+}, {
+    // Other model options go here
+});
+async function seqsync() {
+    await sequelize.sync({force: true});
+    console.log("Все модели польностью синхронизированы.");
+    try {
+        var itsTime = new Date()
+        const user = User.create({
+            id: 1,
+            name: "Kirill",
+            phone: "+79091049890",
+            createAt: itsTime
+        });
+        console.log(User === sequelize.models.User);
+    }
+    catch (err){
+        console.log(err)
+    }
+}
+seqsync();
 
 
 dotenv.config();
@@ -31,48 +79,86 @@ dotenv.config();
 const logger = log4js.getLogger();
 logger.level = process.env.LOG_LEVEL;
 
-
-
 logger.info('log4js log info');
 logger.debug('log4js log debug');
 logger.error('log4js log error');
-
 
 const app = express();
 const port = process.env.PORT;
 
 const urlencodedParser = express.urlencoded({extended: false});
-const users = [];
+const userses = [];
 
-connection.query("SELECT * FROM my_db.employees",
-    function(err, results, fields) {
-        console.log(err);
-        console.log(results); // собственно данные
-        console.log(fields); // мета-данные полей
-    });
-connection.end();
+// connection.query("SELECT * FROM my_db.users",
+//     function(err, results, fields) {
+//         console.log(err);
+//         console.log(results); // собственно данные
+//         console.log(fields); // мета-данные полей
+//     });
+// connection.end();
 
-app.post('/', urlencodedParser,(request, response) => {
-    const { name, email } = request.body;
+app.post('/', urlencodedParser, (request, response) => {
+    const {
+        name,
+        email
+    } = request.body;
 
-    const newUser = { id: 3, name, email };
+    const newUser = {
+        id: 3,
+        name,
+        email
+    };
 
-    users.push(newUser)
+    userses.push(newUser)
     response.status(201).json(newUser);
 });
 
-app.get('/', (request, response) => {
-    response.json(users);
+app.get('/users', (request, response) => {
+    getUsers();
+    response.json(userses);
+
 });
 
 app.delete('/', (request, response) => {
-users.splice(0,1);
-response.send("Удалили элемент")
+    userses.splice(0, 1);
+
+    response.send("Удалили элемент")
 });
 
 app.put('/', (request, response) => {
-    response.json(users);
+    response.json(userses);
 });
 
-app.listen(port, () => console.log(`Running on port ${port}`));
-//
+app.listen(port, () => console.log(`Запуск на порту: ${port}`));
+
+async function getUsers() {
+    try {
+        // Получение всех пользователей
+        const users = await User.findAll();
+        console.log('Users:', users.map(user => user.toJSON()));
+        userses.push(users);
+    } catch (error) {
+        console.error('Ошибка получения пользователей:', error);
+    } finally {
+        // Закрытие подключения к базе данных
+        await sequelize.close();
+    }
+}
+
+function getUserById() {
+
+}
+
+function createUser() {
+
+}
+
+function updateUser() {
+
+}
+
+function deleteUser() {
+
+}
+
+
